ca091fcc67c380aed5b01042a701e7ba524c8dfc
Blanca Sanchez <bsanchez64@cps.edu>
2025-02-05 12:12:19 -0600

    2/4/25

--- ArraysLab1/main.swift
+++ ArraysLab1/main.swift
@@ -104,16 +104,17 @@ There are lots of ways to manipulate arrays using methods and index calling, the
 
 print("***PROBLEM 1.0 : BASIC ARRAY CREATION")
 //  Assume you are an event coordinator for a community charity event and are keeping a list of who has registered. Create a variable registrationList that will hold strings. It should be empty after initialization. Print the empty list and observe the output in the console. Hint: line 20
- 
+var registrationList : [String] = []
+print(registrationList)
 
  print("\n")
 
 print("***PROBLEM 1.1 : BASIC ARRAY ADDITION")
 // Your friend Sara is the first to register for the event. Add her name to registrationList using the append(_:) method. Print the contents of the collection.
 //Hint: See lines 35 & 48
- 
 
- 
+registrationList.append("Sara")
+ print(registrationList)
 print("\n")
 
 print("***PROBLEM 1.2 : MORE ARRAY ADDITIONS")
@@ -123,6 +124,9 @@ print("***PROBLEM 1.2 : MORE ARRAY ADDITIONS")
 // 2. Then add that new array to your original array. Yes ADD.
 //How? To add in multiple names in one step use the += operator we will need to have the names in a separate array that contains the names. Then print your registrationList and observe the output in the console.
 
+var moreNames : [String] = ["Mike", "Charles", "Jack", "Finn"]
+registrationList += moreNames
+print(registrationList)
 
  print("\n")
  
