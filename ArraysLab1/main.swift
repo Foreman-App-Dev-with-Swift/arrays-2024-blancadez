06f05a76c266797ad5acd7bf7ab75434eb827fde
Blanca Sanchez <bsanchez64@cps.edu>
2025-02-10 12:14:16 -0600

    2/10/25

--- ArraysLab1/main.swift
+++ ArraysLab1/main.swift
@@ -185,7 +185,8 @@ print("\n")
 
 print("PROBLEM 2.1 : FITNESS TRACKING")
 // In your app you want to show all of these lists on the same screen grouped into sections. Create a challenges array that holds (assigned the value) each of the lists you have created (it will be an array of arrays). Using the new challenges array, print the first element. Remember what number the first element it. Hint: it's alway minus 1.
-
+var challenges : [String] = walkingChallenge + runningChallenge + lifitngChallenge
+print(challenges[0])
 
 
 print("\n")
@@ -195,7 +196,7 @@ print("***PROBLEM 2.2 : PRINTING FITNESS TRACKING")
 //Create the 2D array to hold the other arrays
 //Confused? You are using a double index [] [] which is calling a list in a list.
 //Print it. The console should say what you coded the second challenge in the second list. If not, figure it out.
-
+print(challenges)
  
  
  
